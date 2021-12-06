78a757ecc25d0554a31f48dfa5e4f05cdd184d13
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2021-12-06 14:27:09 +0000

    Further development of pages.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,31 +1,33 @@
-import React, {useEffect} from 'react';
-import IPage from "../interfaces/page";
-import logging from "../config/logging";
-import {createTheme, styled, ThemeProvider} from '@mui/material/styles';
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
-import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import {mainListItems, secondaryListItems} from './listItems';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { mainListItems, secondaryListItems } from './listItems';
+// import Chart from './Chart';
+// import Deposits from './Deposits';
 import Orders from './Orders';
 
 function Copyright(props: any) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <Link color="inherit" href="https://localhost:3000/">
-                MPetrovProductions
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
@@ -85,12 +87,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 
 const mdTheme = createTheme();
 
-const HomePage: React.FunctionComponent<IPage> = props => {
-
-    useEffect(() => {
-        logging.info(`Loading ${props.name}`);
-    }, [props.name])
-
+function DashboardContent() {
     const [open, setOpen] = React.useState(true);
     const toggleDrawer = () => {
         setOpen(!open);
@@ -98,8 +95,7 @@ const HomePage: React.FunctionComponent<IPage> = props => {
 
     return (
         <ThemeProvider theme={mdTheme}>
-            <Box
-                sx={{ display: 'flex' }}>
+            <Box sx={{ display: 'flex' }}>
                 <CssBaseline />
                 <AppBar position="absolute" open={open}>
                     <Toolbar
@@ -126,8 +122,13 @@ const HomePage: React.FunctionComponent<IPage> = props => {
                             noWrap
                             sx={{ flexGrow: 1 }}
                         >
-                            Home Page
+                            Dashboard
                         </Typography>
+                        <IconButton color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
                     </Toolbar>
                 </AppBar>
                 <Drawer variant="permanent" open={open}>
@@ -163,8 +164,6 @@ const HomePage: React.FunctionComponent<IPage> = props => {
                     <Toolbar />
                     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
                         <Grid container spacing={3}>
-
-
                             {/* Recent Orders */}
                             <Grid item xs={12}>
                                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
@@ -177,7 +176,9 @@ const HomePage: React.FunctionComponent<IPage> = props => {
                 </Box>
             </Box>
         </ThemeProvider>
-    )
+    );
 }
 
-export default HomePage;
+export default function Dashboard() {
+    return <DashboardContent />;
+}
